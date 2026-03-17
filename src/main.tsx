0b11cb7ef93df6ad11f1854fb00eefcec02d53bf
reoxb <suarzs@gmail.com>
2026-03-17 01:31:29 -0600

    updateing name screen-sp500

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './valuation-dashboard.tsx'
+import App from './screener-sp500.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
